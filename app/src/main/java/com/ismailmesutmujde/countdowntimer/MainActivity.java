7802a5d48029e1a84fed2bafc13074e58c1f8344
ismailmesut <ismailmesutmujde@gmail.com>
2024-08-25 11:07:13 +0300

    updated

--- app/src/main/java/com/ismailmesutmujde/countdowntimer/MainActivity.java
+++ app/src/main/java/com/ismailmesutmujde/countdowntimer/MainActivity.java
@@ -1,15 +1,37 @@
 package com.ismailmesutmujde.countdowntimer;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textView = findViewById(R.id.textView);
+
+        new CountDownTimer(10000,1000) {
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                textView.setText("Left : " + millisUntilFinished / 1000);
+            }
+
+            @Override
+            public void onFinish() {
+                Toast.makeText(getApplicationContext(), "Done!", Toast.LENGTH_LONG).show();
+                textView.setText("Finished!");
+            }
+
+        }.start();
     }
 }
